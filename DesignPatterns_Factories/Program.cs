72c8978f2b424318ce62e3c19820b5eb40386e1e
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-09 19:21:52 -0600

    Reproduce dilemma in cartesian or polar coordinate systems example

--- DesignPatterns_Factories/Program.cs
+++ DesignPatterns_Factories/Program.cs
@@ -4,15 +4,39 @@ namespace DesignPatterns_Factories
     // Why we need Factories design pattern?
     // Point Example
 
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
     public class Point
     {
         private double x, y;
+        /// <summary>
+        /// Initializes a point from EITHER cartesian or polar 
+        /// </summary>
+        /// <param name="a"> x if cartesian, rho if polar</param>
+        /// <param name="b"> y if cartesian, theta if polar</param>
+        /// <param name="system"></param>
 
         // Cartesian coordinate system:
-        public Point(double x, double y)
+        public Point(double a, double b, 
+            CoordinateSystem system = CoordinateSystem.Cartesian)
         {
-            this.x = x;
-            this.y = y;
+            switch (system)
+            {
+                case CoordinateSystem.Cartesian:
+                    x = a;
+                    y = b;
+                    // also confusing whether a is x or y 
+                    break;
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = a * Math.Sin(b);
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
         }
 
         // imagine you also need to use a Polar coordinate system
@@ -25,6 +49,9 @@ namespace DesignPatterns_Factories
         //{
         //    // the signature is exactly the same
         //}
+
+        // you can introduce coordinate types so that you can pass it to constructor
+        // but then there is a naming problem as x and y implies cartesian system
 
     }
     internal class Program
